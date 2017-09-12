323a36e204e701cba7f295010b7cd32d4b827252
Keane O'Kelley <keane.m.okelley@gmail.com>
2017-09-12 00:39:36 -0500

    Some work

--- multi_handler_server.py
+++ multi_handler_server.py
@@ -1,8 +1,9 @@
 import argparse
-import datetime
+import random
 import socket
-
 from multiprocessing import Process
+
+import datetime
 
 parser = argparse.ArgumentParser(description="Reverse shell multi-handler")
 parser.add_argument('--control-port', type=int, default=5555, help="TCP port to use for the "
@@ -12,8 +13,9 @@ parser.add_argument('--shell-port', type=int, default=4444, help="TCP port to us
 
 args = parser.parse_args()
 
-sessions = {}
+sessions = []
 server_started = datetime.datetime.now()
+PROMPT = '=> '
 
 
 def total_sessions() -> int:
@@ -27,21 +29,26 @@ class Connection(Process):
     def __init__(self, conn: socket.socket, addr: str):
         self.conn = conn
         self.addr = addr
+        super().__init__()
 
-    def run(self):
-        welcome = "Server started at {}. {} session{} active.".format(
+    def conni(self):
+        welcome = "Server started at {}. {} session{} active.\n".format(
             server_started,
             len(sessions),
-            "s" if len(sessions) else "")
-        self.conn.send(welcome)
-        welcome = "Type (l) to list sessions, and (i #) to interact with a session."
-        self.conn.send(welcome)
-        r = self.conn.recv(100).decode()
-        if r == 'l':
-            session_list = "Active sessions:\n"
-            for n, session in enumerate(sessions):
-                session_list += "({}) {}\n".format(n, session.addr)
-            self.conn.send(session_list)
+            "s" if not len(sessions) == 1 else "")
+        self.conn.send(welcome.encode())
+        welcome = "Type (l) to list sessions, and (i) to interact with a session.\n"
+        self.conn.send(welcome.encode())
+        while True:
+            self.conn.send(PROMPT.encode())
+            r = self.conn.recv(1024).decode().strip()
+            if r == 'l':
+                session_list = "Active sessions:\n"
+                for n, session in enumerate(sessions):
+                    session_list += "({}) {}\n".format(n, session[0])
+                self.conn.send(session_list.encode())
+            elif r.startswith('i'):
+
 
 
 class StoppableServer(Process):
@@ -63,34 +70,37 @@ class ShellServer(StoppableServer):
     def __init__(self, host: str="0.0.0.0", port: int=4444):
         super().__init__(host, port)
 
-    def run(self):
+    def run(self) -> None:
         print("Listening for shell connections")
         while not self.stopped():
             conn, addr = self.shell_sock.accept()
-            try:
-                sessions[addr].append(conn)
-            except TypeError:
-                sessions[addr] = [conn]
+            sessions.append((addr, conn))
         self.sock.close()
 
 
 class ControlServer(StoppableServer):
     def __init__(self, host: str ='127.0.0.1', port: int=5555):
+        self.connections = []
         super().__init__(host, port)
 
     def serve(self) -> None:
         print("Listening for control connections...")
         while not self.stopped():
-            Connection(*self.sock.accept()).start()
+            c = Connection(*self.sock.accept())
+            self.connections.append(c)
+            c.conni()
         self.sock.close()
 
 
 if __name__ == '__main__':
-    #ss = ShellServer(port=args.shell_port)
-    cs = ControlServer(port=args.control_port)
+    ss = ShellServer(port=args.shell_port)
+    rand = random.randint(1024, 65535)
+    print(rand)
+    cs = ControlServer(port=rand)  # args.control_port)
     try:
         cs.serve()
-    #    ss.start()
-    except KeyboardInterrupt:
+        ss.start()
+    except (KeyboardInterrupt, Exception) as e:
         cs.stop()
-    #    ss.stop()
+        ss.stop()
+        raise e
